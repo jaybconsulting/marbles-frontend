8018d1796432cee48112be8079e79bb4e7848c36
jdnbo89 <hello@jayboychuk.com>
2025-02-19 08:04:09 +0700

    feat(navbar): add navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
 
-export default function Layout() {
+const Layout = () => {
     return (
-        <Outlet />
+        <>
+            <Navbar />
+            <Outlet />
+        </>
     );
-}
+};
+
+export default Layout;
