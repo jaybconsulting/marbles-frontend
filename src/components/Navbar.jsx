c64155f6f5087be7f52406b9c76d2870accfc5b3
jdnbo89 <hello@jayboychuk.com>
2025-02-19 08:03:56 +0700

    feat(navbar): add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+import { 
+  Avatar,
+  Box, 
+  Button,
+  Flex, 
+  HStack,
+  Text,
+} from '@chakra-ui/react';
+import { FaPlus } from 'react-icons/fa';
+import {
+  MenuRoot,
+  MenuTrigger,
+  MenuContent,
+  MenuItem,
+} from './ui/menu';
+import CreateGameDialog from './CreateGameDialog';
+
+
+const Navbar = () => {
+  const [isCreateGameDialogOpen, setIsCreateGameDialogOpen] = useState(false);
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate('/login');
+  };
+
+  const handleCreateGame = () => {
+    setIsCreateGameDialogOpen(true);
+  };
+
+  return (
+    <>
+      <Box 
+        bg='bg.muted'
+        px={4}
+      >
+        <Flex 
+          h={16}
+          alignItems='center'
+          justifyContent='space-between'
+        >
+          <Text fontSize='2xl' fontWeight='bold'>
+            Marbles
+          </Text>
+          {user && 
+            <HStack>
+              <Button
+                padding={2}
+                size='sm'
+                aria-label='Create new game'
+                onClick={handleCreateGame}
+                mr={2}
+              >
+                <FaPlus /> Create new game
+              </Button>
+            
+              <MenuRoot>
+                <MenuTrigger asChild>
+                  <Button
+                    variant='ghost'
+                    p={0}
+                    border='none'
+                    focusRing='none'
+                  >
+                    <Avatar.Root>
+                      <Avatar.Image src={user?.picture} />
+                      <Avatar.Fallback name={`${user?.firstName} ${user?.lastName}`} />
+                    </Avatar.Root>
+                  </Button>
+                </MenuTrigger>
+                <MenuContent
+                  bg='gray'
+                >
+                  <MenuItem
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </MenuItem>
+                </MenuContent>
+              </MenuRoot>
+            </HStack>
+          }
+        </Flex>
+      </Box>
+      <CreateGameDialog 
+        isOpen={isCreateGameDialogOpen} 
+        setIsOpen={setIsCreateGameDialogOpen}
+      />
+    </>
+  );
+};
+
+export default Navbar; 
